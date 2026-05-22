b80353b772c24c005f3b41acac05b1209da380a8
Gustavo Diaz <gusdiaz7722@gmail.com>
2026-05-22 21:58:44 +0000

    fix

--- prow/jobs/tools/cmd/build-prow-images.sh
+++ prow/jobs/tools/cmd/build-prow-images.sh
@@ -33,9 +33,14 @@ if [ -z "$PROW_ECR_REPO_NAME" ]; then
 fi
 echo "build-prow-images.sh] [SETUP] Using ECR repo: $PROW_ECR_REPO_NAME"
 
+# Resolve env vars in config files
+envsubst < ./prow/jobs/images_config.yaml > /tmp/images_config.yaml
+envsubst < ./prow/plugins/images_config.yaml > /tmp/plugins_images_config.yaml
+envsubst < ./prow/agent-workflows/images_config.yaml > /tmp/agent_workflows_images_config.yaml
+
 # Build Prow Jobs
 BUILT_JOB_TAGS=$(ack-build-tools build-prow-images \
-  --images-config-path ./prow/jobs/images_config.yaml \
+  --images-config-path /tmp/images_config.yaml \
   --jobs-config-path ./prow/jobs/jobs_config.yaml \
   --jobs-templates-path ./prow/jobs/templates/ \
   --jobs-output-path ./prow/jobs/jobs.yaml \
@@ -48,7 +53,7 @@ fi
 
 # Build Prow Agent Workflows
 BUILT_AGENT_WORKFLOW_TAGS=$(ack-build-tools build-prow-agent-workflow-images \
-  --images-config-path ./prow/agent-workflows/images_config.yaml \
+  --images-config-path /tmp/agent_workflows_images_config.yaml \
   --prow-ecr-repository "$PROW_ECR_REPO_NAME" \
   --agent-workflows-templates-path ./prow/agent-workflows/templates \
   --agent-workflows-output-path ./prow/agent-workflows/agent-workflows.yaml)
@@ -60,7 +65,7 @@ fi
 
 # Build Prow Plugins
 BUILT_PLUGIN_TAGS=$(ack-build-tools build-prow-plugin-images \
-  --images-config-path ./prow/plugins/images_config.yaml \
+  --images-config-path /tmp/plugins_images_config.yaml \
   --prow-ecr-repository "$PROW_ECR_REPO_NAME")
 
 if [ $? -ne 0 ]; then
